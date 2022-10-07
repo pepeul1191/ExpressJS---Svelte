ef57420a914fc5e6a3ad7e33b4f3a7396f5ada0f
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2022-10-06 22:23:52 -0500

    base de datos agregada

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-var express = require('express');
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(8000, function () {
+app.get('/pokemons/list', (req, res) => {
+  let db = new sqlite3.Database('./pokemons.db');
+  let sql = 'SELECT * FROM pokemons LIMIT 20';
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    db.close();
+    res.send(rows)
+  });
+});
+
+app.listen(8000, () => {
   console.log('Listening to Port 8000');
 });
