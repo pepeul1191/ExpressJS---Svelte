e44b5e1e7bc6e1d25879a192915e317f2b1693fc
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-02-21 20:15:42 -0500

    path params

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   var locals = {};
@@ -16,14 +15,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/pokemon/list', (req, res) => {
-  let db = new sqlite3.Database('./pokemons.db');
+  const db = new sqlite3.Database('./pokemons.db');
   let sql = 'SELECT * FROM pokemons LIMIT 20';
   db.all(sql, [], (err, rows) => {
     if (err) {
+      console.error(err);
       throw err;
     }
     db.close();
     res.send(rows)
+  });
+});
+
+app.get('/pokemon/:id', (req, res) => {
+  const db = new sqlite3.Database('./pokemons.db');
+  let sql = `SELECT * FROM pokemons WHERE number=${req.params.id}`;
+  db.get(sql, [], (err, row) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('ups, ocurrió un error');
+    }
+    db.close();
+    res.send(row)
   });
 });
 
